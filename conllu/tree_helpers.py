2a9d1abf6f40525a514326cf12465767b1224716
Emil Stenström <em@kth.se>
2016-08-28 14:57:04 +0200

    Simplify print_tree usage.

--- conllu/tree_helpers.py
+++ conllu/tree_helpers.py
@@ -1,3 +1,4 @@
+from __future__ import print_function, unicode_literals
 from collections import namedtuple
 
 TreeNode = namedtuple('TreeNode', ['data', 'children'])
@@ -9,12 +10,14 @@ def create_tree(node_children_mapping, start=0):
     ]
     return subtree
 
-def print_tree(tree, depth=0):
-    for child in tree.children:
-        print "\t" * depth + "(deprel:{deprel}) form:{form}, tag:{tag} [{idx}]".format(
-            deprel=child.data["deprel"],
-            form=child.data["form"],
-            tag=child.data["upostag"],
-            idx=child.data["id"],
-        )
-        print_tree(child.children, depth + 1)
+def print_tree(node, depth=0):
+    assert isinstance(node, TreeNode), "node not TreeNode %s" % type(node)
+
+    print("\t" * depth + "(deprel:{deprel}) form:{form}, tag:{tag} [{idx}]".format(
+        deprel=node.data["deprel"],
+        form=node.data["form"],
+        tag=node.data["upostag"],
+        idx=node.data["id"],
+    ))
+    for child in node.children:
+        print_tree(child, depth + 1)
