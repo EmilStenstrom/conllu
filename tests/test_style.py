b2629c938e96956f80099e022d86cf64a0cc6b4c
Roman Inflianskas <infroma@gmail.com>
2017-02-23 19:00:46 +0100

    Update tests

--- tests/test_style.py
+++ tests/test_style.py
@@ -6,4 +6,4 @@ class TestFlake8Compliance(unittest.TestCase):
     def test_flake8(self):
         style_guide = get_style_guide()
         report = style_guide.check_files()
-        self.assertEquals(report.get_statistics('E'), [])
+        self.assertEqual(report.get_statistics('E'), [])
