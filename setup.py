526676afc17c511843bca3af0fc571b6b64f4604
Emil Stenström <em@kth.se>
2018-04-13 16:51:07 +0200

    Add Travis CI and specify Python 3 versions.

--- setup.py
+++ setup.py
@@ -20,6 +20,10 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
         "Operating System :: OS Independent",
     ],
 )
