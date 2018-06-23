5dd070c1ff232a35b8b6b5a308ba7a453eb41683
Emil Stenström <em@kth.se>
2018-06-23 21:43:40 +0200

    Ignore unused import errors, they are intended.

--- conllu/__init__.py
+++ conllu/__init__.py
@@ -1,2 +1,2 @@
-from conllu.parser import parse, parse_tree, serialize_tree
-from conllu.tree_helpers import print_tree
+from conllu.parser import parse, parse_tree, serialize_tree  # noqa
+from conllu.tree_helpers import print_tree # noqa
