774861ff72bfc8f044d086131a7c545cae31bce1
Emil Stenström <em@kth.se>
2018-08-04 17:54:38 +0200

    Fix flake8 errors.

--- conllu/parser.py
+++ conllu/parser.py
@@ -34,14 +34,14 @@ def parse_with_comments(text, fields=DEFAULT_FIELDS):
                 if line:
                     if line.strip().startswith("#"):
                         var_name, var_value = parse_comment_line(line)
-                        if var_name != None:
+                        if var_name:
                             metadata[var_name] = var_value
                     else:
                         lemmas.append(parse_line(line, fields))
             sentences.append(OrderedDict([
                 ('metadata', metadata),
                 ('lemmas', lemmas)
-            ]))   
+            ]))
     return sentences
 
 def sent_to_tree(sentence):
@@ -106,8 +106,8 @@ def parse_line(line, fields=DEFAULT_FIELDS):
 def parse_comment_line(line):
     line = line.strip()
     if line[0] != '#':
-        raise ParseException("Invalid comment format, comment must contain '#' character in begin of line.")
-    if not '=' in line:
+        raise ParseException("Invalid comment format, comment must start with '#'")
+    if '=' not in line:
         return None, None
     var_name, var_value = line[1:].split('=', 1)
     var_name = var_name.strip()
