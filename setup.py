1bc326e065fb9580408fe9e78282f22c00d5d376
Emil Stenström <em@kth.se>
2020-08-15 10:28:17 +0200

    Remove old Python pypi classifiers.

--- setup.py
+++ setup.py
@@ -19,10 +19,8 @@ setup(
     keywords=['conllu', 'conll', 'conll-u', 'parser', 'nlp'],
     classifiers=[
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
