778c23c541374e56e9a04576076278565e8e55fb
Emil Stenström <emil@emilstenstrom.se>
2022-07-29 09:50:01 +0200

    Bump version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 
 from setuptools import setup  # type: ignore
 
-VERSION = '4.5.1'
+VERSION = '4.5.2'
 
 with open(os.path.join(os.path.dirname(__file__), 'README.md')) as f:
     description = f.read()
